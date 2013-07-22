7492ea9450ae25d1ce78cc67fbe6cfa64a9d7c41
Gerardo J. Vasquez R. <vasquezgerardojr@gmail.com>
2013-07-22 19:08:56 -0430

    Update views.py

--- views.py
+++ views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 import datetime
 
 def current_datetime(request):
+	
 	now = datetime.datetime.now()
 	html = "<html><boby>It is now%s.</body></html>"% now
+	
 	return HttpResponse(html)
